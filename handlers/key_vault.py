bb8faad640632b23ccdcd5957f51c1aba122b114
Paul Nagy <7782463+paulnagy@users.noreply.github.com>
2022-06-02 11:07:27 -0400

    Delete key_vault.py

--- handlers/key_vault.py
+++
@@ -1,9 +0,0 @@
-def get_key_dict():
-  key_dict={'YOUTUBE_API_SERVICE_NAME':'youtube',
-    'YOUTUBE_API_VERSION':'v3',
-    'YOUTUBE_DEVELOPER_KEY':'AIzaSyCJ-ol-tP92iyAFz1cqey5qrep_mO__qtg',
-    'AZURE_ENDPOINT':'https://bids-classdb.documents.azure.com:443',
-    'AZURE_KEY':"0xZTgw39TMFEKvEhImf8iygj3VclZRlTiZacOrdiGugu852mD9ItAI8IVIn6AiU1ogMW8THJ9iZBzbeNWt6UFA==",
-    'SERPAPI_KEY':"a9e5f62231725d41ebbdb6e56ffa08790b155db10e07bc09dab6207c9d6cd02f"
-    }
-  return key_dict
