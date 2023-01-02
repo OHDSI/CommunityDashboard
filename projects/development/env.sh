3774061c749d2bf98116681be71a906dd6180825
Nathan Buesgens <nathan@natb1.com>
2023-01-02 13:11:00 +0000

    fix codespace cors issue

--- projects/development/env.sh
+++ projects/development/env.sh
@@ -1,3 +1,4 @@
 if [ -n $GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN ]; then
   export NG_ALLOWED_HOSTS="--allowed-hosts ${CODESPACE_NAME}-4200.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN"
+  gh codespace ports visibility 5001:public -c $CODESPACE_NAME # https://github.com/community/community/discussions/15351
 fi
