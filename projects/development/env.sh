7181b6de529e3f029b28fa1aa677ea22ce91b4cb
nate <nathan@natb1.com>
2023-03-14 12:01:58 -0400

    testing port config for dev container

--- projects/development/env.sh
+++ projects/development/env.sh
@@ -1,4 +1,5 @@
 if [ -n $GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN ]; then
   export NG_ALLOWED_HOSTS="--allowed-hosts ${CODESPACE_NAME}-4300.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN"
   gh codespace ports visibility 5001:public -c $CODESPACE_NAME # https://github.com/community/community/discussions/15351
+  gh codespace ports visibility 4300:public -c $CODESPACE_NAME # https://github.com/community/community/discussions/15351
 fi
